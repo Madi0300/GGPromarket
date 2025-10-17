34d4157d866ccc37ffe5dda8dd01f949616d1364
Madi Aitbai <madibro999@gmail.com>
2025-10-17 11:30:20 +0500

    refactor: refactor routes for dynamic modal component

--- client/app/routes.ts
+++ client/app/routes.ts
@@ -1,5 +1,17 @@
-import { type RouteConfig, index, layout } from "@react-router/dev/routes";
+import {
+  type RouteConfig,
+  index,
+  layout,
+  route,
+} from "@react-router/dev/routes";
 
 export default [
-  layout("routes/headerBoard/HeaderBoard.tsx", [index("routes/Home/home.tsx")]),
+  layout("routes/headerBoard/HeaderBoard.tsx", [
+    route("/", "routes/Home/home.tsx", [
+      route("product/:productId", "routes/Modal/Modal.tsx", {
+        id: "product-detail",
+      }),
+    ]),
+  ]),
+  route("*", "routes/NotFound/NotFound.tsx"),
 ] satisfies RouteConfig;
