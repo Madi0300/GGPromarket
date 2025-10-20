30347bcc208ce9e7aea8bbdc2f0fe7650a412f71
Madi Aitbai <madibro999@gmail.com>
2025-10-20 12:20:17 +0500

    fix: fix cors origin

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(
   cors({
-    origin: ["https://madi0300.github.io/", "http://localhost:5173"],
+    origin: ["https://madi0300.github.io", "http://localhost:5173"],
   })
 );
 app.use(express.json());
