41b3481a6bffc33b551185f2b610a1ec995ab832
Madi Aitbai <madibro999@gmail.com>
2025-10-20 12:14:18 +0500

    refactor: add env for dynamic link to server

--- client/app/store/apiSlise.ts
+++ client/app/store/apiSlise.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
 
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3001/api" }),
+  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_API_BASE_URL }),
 
   endpoints: (builder) => ({
     getServerUrl: builder.query({
