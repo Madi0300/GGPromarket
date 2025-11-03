04ec7d237e080d4279e6805876b65fa2c03111a2
Madi Aitbai <madibro999@gmail.com>
2025-11-04 00:14:28 +0500

    Updates

---
+++ assets/Icons-4reMaJOC.js
@@ -0,0 +1,6 @@
+import{p as n,R as i}from"./chunk-UH6JLGW7-BuU8Z2XH.js";const _="_Icons_1d0jc_1",r="_Icons__item_1d0jc_15",a="_Icons__content_1d0jc_40",s={Icons:_,Icons__item:r,Icons__content:a},l=[{key:"truck",icon:"Icons/truck.svg",text:`Быстрая доставка
+по Москве и в любой
+регион России`},{key:"shield",icon:"Icons/shield.svg",text:`Все товары имеют
+официальную гарантию
+производителя`},{key:"forklift",icon:"Icons/forklift.svg",text:"Более 100 000 товаров"},{key:"coin",icon:"Icons/coin.svg",text:"Любой способ оплаты"},{key:"like",icon:"Icons/like.svg",text:"Высокие оценки клиентов"}];function x({content:o=l}){return n.jsx("div",{className:s.Icons,children:o.map(c=>n.jsxs("div",{className:s.Icons__item,children:[n.jsx("div",{className:s.Icons__iconWrapper,children:n.jsx("img",{className:s.Icons__iconWrapper__img,src:"/GGPromarket/"+c.icon,alt:c.key})}),n.jsx("p",{className:s.Icons__content,children:c.text.split(`
+`).map((t,e)=>n.jsxs(i.Fragment,{children:[t,n.jsx("br",{})]},e))})]},c.key))})}export{x as default,l as iconsData};
---
+++ assets/hooks-ZVFlrzEq.js
@@ -0,0 +1,1 @@
+import{f as s,g as e}from"./apiSlise-DyPyVMm8.js";const t=()=>e(),a=s;export{t as a,a as u};
